def43a3648017efdac3c21e3ccec611fbf2988dc
TNY6918 <antonsave69@gmail.com>
2024-02-07 10:27:30 +0200

    adding changes to hw2

--- cypress/e2e/param_test_hw2.cy.js
+++ cypress/e2e/param_test_hw2.cy.js
@@ -1,59 +1,64 @@
 before(() => {
-  cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com');
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com');
 
-  cy.log('Get the 3rd element with header text Light and click on it');
-  cy.get('nb-card:nth-of-type(3) nb-card-header').contains('Light').click();
+    cy.log('Get the 3rd element with header text Light and click on it');
+    cy.get('nb-card:nth-of-type(3) nb-card-header').contains('Light').click();
 
-  cy.log('Get the side-menu Modal and Overlays drop-dowm menu open');
-  cy.get('a.ng-tns-c141-19').click();
+    cy.log('Get the side-menu Modal and Overlays drop-dowm menu open');
+    cy.get('a.ng-tns-c141-19').click();
 
-  cy.log('Get the side menu button Form Layout from the drop down menu and click on it');
-  cy.get('a.ng-tns-c141-23').click();
+    cy.log('Get the side menu button Form Layout from the drop down menu and click on it');
+    cy.get('a.ng-tns-c141-23').click();
 
 })
 
-describe(`Check of the toast messages`, () => {
-  let testDataSet1 = [{
-    "title": "Success",
-    "content": "It is a success message!",
-    "timeShow": 5000
-  }]
+describe ('test toast', () => {
+    const tableTestData = [
+        {
+            inputTest: {
+                position: 'top-right',
+                title: 'test title 1',
+                content: 'test content 1',
+                time: '10000',
+                type: 'primary'
+            },
+            expectedTest: {
+                icon: 'email',
+                title: 'test title 1',
+                content: 'test content 1',
+                color: 'rgb(51, 102, 255)',
+                position: {
+                    justifyContent: "flex-end",
+                    alignItems: "flex-start"
+                }
+            }
+        }
+    ];
 
-  it('setting the success toast', () => {
-    testDataSet1.forEach(({title, content, timeShow }) =>{
-      cy
-          .get('nb-select.mat-ripple.position-select')
-          .click();
-      cy
-          .get('#nb-option-24')
-          .click(); //як правильно прописати в тестДатаСет1 значення айдішника,
-      cy
-          .get('[name="title"]')
-          .clear()
-          .type(`${title}`);
-      cy
-          .get('[name="content"]')
-          .clear()
-          .type(`${content}`);
-      cy
-          .get('[name="timeout"]')
-          .clear()
-          .type(`${timeShow}`);
-      cy.get()
-      cy
-          .get('button.mat-ripple.appearance-filled.size-medium.shape-rectangle.status-basic.nb-transition')
-          .should('contain', 'Show toast')
-          .click()
-          .then(toast => {
-        expect(toast).to.have.id('cdk-overlay-0');
-        expect(toast).to.have.attr('')
-      })
+    it('Create toast', () => {
+        cy.get('div.col-md-6.col-sm-12 button.select-button').eq(0).click();
+        cy.get(`nb-option[ng-reflect-value="${tableTestData[0].inputTest.position}"]`).click();
+        cy.get(`input[name="title"]`).clear().type(`${tableTestData[0].inputTest.title}`);
+        cy.get(`input[name="content"]`).clear().type(`${tableTestData[0].inputTest.content}`);
+        cy.get(`input[name="timeout"]`).clear().type(`${tableTestData[0].inputTest.time}`);
+        cy.get('div.col-md-6.col-sm-12 button.select-button').eq(1).click();
+        cy.get(`nb-option[ng-reflect-value="${tableTestData[0].inputTest.type}"]`).click();
+        cy.contains('button', 'Show toast').click();
+
+        cy.get(`div.icon-container.ng-star-inserted g`).eq(1).then(toast => {
+            expect(toast).to.have.attr('data-name', `${tableTestData[0].expectedTest.icon}`);
+        })
+        cy.get('div.content-container span').then(toast => {
+            expect(toast).to.have.class('title subtitle');//.to.equal(`${tableTestData[0].expectedTest.title}`); - не розумію чому не працює
+        })
+        cy.get('div.content-container div').then(toast => {
+            expect(toast).to.have.class('message');
+        })
+        //cy.get('#cdk-overlay-0').invoke('css', 'background-color').as('backgroundColor');
+        //cy.get('@backgroundColor').should('eq', `${tableTestData[0].expectedTest.color}`); - я так розумію тут також має бути then але як побудувати це із then
+        cy.get(`div [dir="ltr"]`).then(toast => {
+            const expectedStyle = `justify-content: ${tableTestData[0].expectedTest.position.justifyContent}; align-items: ${tableTestData[0].expectedTest.position.alignItems};`;
+            expect(toast).to.have.attr('style', expectedStyle); //можливо можна якось краще це записати?
+        })
     })
-  })
-})
-
-describe('template spec', () => {
-  it('passes', () => {
-
-  })
 })
